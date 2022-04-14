b0fdc4ffbea64541cf15f8435d88f639d6481886
Ashish Kumar Bhoi <ashish.bhoi12@gmail.com>
2022-04-14 17:57:25 +0530

    Button style changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,48 @@
-import { useState } from 'react'
+import {useState} from 'react'
 import logo from './logo.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0)
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p className="text-4xl font-bold">Hello Vite + React!</p>
+                <p>
+                    <button type="button" className="inline-flex items-center mt-3 px-4 py-2 border border-transparent
+                    rounded-md shadow-sm text-3xl font-medium text-white bg-indigo-600 hover:bg-indigo-700
+                    focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                            onClick={() => setCount((count) => count + 1)}>
+                        Count is: {count}
+                    </button>
+                </p>
+                <p>
+                    Edit <code>App.jsx</code> and save to test HMR updates.
+                </p>
+                <p>
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Learn React
+                    </a>
+                    {' | '}
+                    <a
+                        className="App-link"
+                        href="https://vitejs.dev/guide/features.html"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Vite Docs
+                    </a>
+                </p>
+            </header>
+        </div>
+    )
 }
 
 export default App
